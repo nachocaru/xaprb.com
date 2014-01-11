6c8649fcff833c8d4a04f038b6cb33807009e410
Baron Schwartz <baron@xaprb.com>
2014-01-11 14:37:18 -0500

    getting ready to munge categories

--- fix.pl
+++ fix.pl
@@ -1,20 +1,101 @@
+my %cat_for= (
+    'About '                 => 'About',
+    'About'                  => 'About',
+    'Aspersa'                => 'Open Source',
+    'C'                      => 'Programming',
+    'CSS'                    => 'Web',
+    'Cacti'                  => 'Monitoring',
+    'Cassandra'              => 'Databases',
+    'Coding'                 => 'Programming',
+    'Commentary'             => 'Commentary',
+    'Conferences'            => 'Conferences',
+    'CouchDB'                => 'Databases',
+    'Deployment'             => 'Operations',
+    'Desktop'                => 'Desktop',
+    'Drizzle'                => 'Databases',
+    'GNU/Linux'              => 'Open Source',
+    'Gadgets'                => 'Hardware',
+    'Go'                     => 'Programming',
+    'Health'                 => 'Health',
+    'High Availability'      => 'High Availability',
+    'High Performance MySQL' => 'Writing',
+    'Innotop'                => 'Open Source',
+    'Internet'               => 'Web',
+    'Javascript'             => 'Web',
+    'Maatkit'                => 'Open Source',
+    'MariaDB'                => 'Databases',
+    'Modeling'               => 'Architecture',
+    'MongoDB'                => 'Databases',
+    'Monitoring'             => 'Monitoring',
+    'Music'                  => 'Music',
+    'Open Source'            => 'Open Source',
+    'OpenSQL Camp'           => 'Conferences',
+    'Operations'             => 'Operations',
+    'Oracle'                 => 'MySQL',
+    'PHP'                    => 'Programming',
+    'Percona Toolkit'        => 'Open Source',
+    'Perl'                   => 'Programming',
+    'PostgreSQL'             => 'Databases',
+    'Review'                 => 'Reviews',
+    'Riak'                   => 'Databases',
+    'SQL'                    => 'Databases',
+    'Scalability'            => 'Performance',
+    'Security'               => 'Security',
+    'Sphinx'                 => 'Databases',
+    'Sys Admin'              => 'Operations',
+    'Tools'                  => 'Open Source',
+    'Uncategorized'          => 'Databases',
+    'Visual Basic'           => 'Programming',
+    'World Wide Web'         => 'Web',
+    'XML'                    => 'Web',
+);
+
 $skip = 0;
-while(<>){
-   if (!$skip) {
-      if ($seen_tags && m/^\S/){
-         $skip++;
-         print; next;
-      }
-      if (m!^tags:!) {
-         $seen_tags++;
-      }
-      if ($seen_tags) {
-         next;
-      } else {
-         print;
-      }
-   }
-   else {
-      print;
-   }
+my %cats;
+my %pats = (
+   'Maatkit' => ['Open Source', 'Databases'],
+);
+while (<>) {
+    if ( !$skip ) {
+       foreach my $pat (keys %pats) {
+          if (m/$pat/ ){
+             $type = $pats{$pat};
+          }
+       }
+        if ( $seen_cats && m/^\s+- (.*$)/ ) {
+           $newcat = $cat_for{$1};
+           die $1 unless $newcat;
+           $cats{$newcat}++;
+           next;
+        }
+        if (m!^categories:!) {
+            $seen_cats++;
+            next;
+        }
+        if (m/---/){
+           $dashes++;
+           if ($dashes == 2) {
+                 print "categories:\n";
+              if (!$seen_cats) {
+                 if (!$type) {
+                    print;
+                    $skip++;
+                    next;
+                 }
+                 $cats{$_}++ foreach @$type;
+              }
+              else {
+                 foreach my $cat (sort keys %cats) {
+                    print "  - $cat\n";
+                 }
+              }
+
+              $skip++;
+           }
+        }
+        print;
+    }
+    else {
+        print;
+    }
 }
